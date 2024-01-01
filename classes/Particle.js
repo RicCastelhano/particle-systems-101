07c3193e00d594b69f75d1b7a7d016231c13e3e0
RicCastelhano <ricardocastelhano@gmail.com>
2024-01-01 15:09:10 +0000

    Render one particle on screen

--- classes/Particle.js
+++ classes/Particle.js
@@ -1,15 +1,17 @@
 class Particle{
     constructor(){
-
+        this.x = 100;
+        this.y = 100;
     }
 
     draw(){
-
-
+        ctx.fillStyle = `white`;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 8, 0, Math.PI * 2);
+        ctx.fill();
     }
 
     update(){
-
-        
+        this.draw();
     }
 }
--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let particle = new Particle();
 
 // Animation Loop
 function animate(){
-
+    particle.update();
     requestAnimationFrame(animate);
 }
 
